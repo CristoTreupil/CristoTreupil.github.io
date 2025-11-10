2a45c41a45df41f6ff3d0cd126ad07fe3d8819e4
Cristobal <cristobal.treupil@gmail.com>
2025-11-10 17:18:34 -0300

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cristotreupil.github.io',
+    site: 'https://CristoTreupil.github.io',
     devToolbar: {
         enabled: false
     },
